1ff2987411dfaad878dc7d5821e9253125028143
Aarne Rissanen <ari.outermedia.de>
2023-01-13 17:31:14 +0100

    remove parsing of legacy date

--- src/prices.mjs
+++ src/prices.mjs
@@ -18,17 +18,10 @@ function createApp(database) {
     const age = req.query.age;
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
-    const date = parseDate(req.query.date);
-    const date2 = parseTemporalDate(req.query.date);
-    const cost = calculateCost(age, type, date2, baseCost);
+    const date = parseTemporalDate(req.query.date);
+    const cost = calculateCost(age, type, date, baseCost);
     res.json({ cost });
   });
-
-  function parseDate(dateString) {
-    if (dateString) {
-      return new Date(dateString);
-    }
-  }
 
   function parseTemporalDate(dateString) {
     if (dateString) {
